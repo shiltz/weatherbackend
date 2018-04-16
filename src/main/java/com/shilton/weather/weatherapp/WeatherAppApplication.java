3f668d75e0fc039e8551166c9f4ab7e250ef4755
Shilton Naicker <shilton.naicker@standardbank.co.za>
2018-04-16 22:04:20 +0200

    add cachemanager to the mbeanserver

--- src/main/java/com/shilton/weather/weatherapp/WeatherAppApplication.java
+++ src/main/java/com/shilton/weather/weatherapp/WeatherAppApplication.java
@@ -1,10 +1,15 @@
 package com.shilton.weather.weatherapp;
 
 import com.shilton.weather.weatherapp.config.StandaloneConfig;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.management.ManagementService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
+
+import javax.management.MBeanServer;
+import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = {StandaloneConfig.class})
@@ -13,5 +18,9 @@ public class WeatherAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WeatherAppApplication.class, args);
+        CacheManager manager = CacheManager.getCacheManager(null);
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ManagementService.registerMBeans(manager, mBeanServer, false, false, false,
+                true);
     }
 }
