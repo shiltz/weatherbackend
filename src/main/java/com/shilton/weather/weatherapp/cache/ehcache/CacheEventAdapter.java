151e83df11075b1aa11b04a621e92b9cc598f250
Shilton Naicker <shilton.naicker@standardbank.co.za>
2018-04-17 12:02:19 +0200

    more tweaks

--- src/main/java/com/shilton/weather/weatherapp/cache/ehcache/CacheEventAdapter.java
+++ src/main/java/com/shilton/weather/weatherapp/cache/ehcache/CacheEventAdapter.java
@@ -12,21 +12,21 @@ public class CacheEventAdapter extends CacheEventListenerAdapter {
 
     @Override
     public void notifyElementPut(Ehcache ehcache, Element element){
-        logger.info("Putting in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
+        logger.debug("Putting in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
     }
 
     @Override
     public void notifyElementEvicted(Ehcache ehcache, Element element){
-        logger.info("Evicted in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
+        logger.debug("Evicted in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
     }
 
     @Override
     public void notifyElementExpired(Ehcache ehcache, Element element){
-        logger.info("Expired in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
+        logger.debug("Expired in cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
     }
 
     @Override
     public void notifyElementRemoved(Ehcache ehcache, Element element){
-        logger.info("Removed from cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
+        logger.debug("Removed from cache: {}, {}", ehcache.getName(), element.getObjectKey().toString());
     }
 }
